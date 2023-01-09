59edf4d5b488d61dc7ef90404c542272063c26b4
LordBenz <dechapon.t@gmail.com>
2023-01-09 15:21:58 +0700

    feat: init web page gathering js game poc tutorials and projects

---
+++ script.js
@@ -0,0 +1,32 @@
+const projectList = [
+  {
+    gameKey: "sprite-sheet-animation",
+    name: "Sprite Sheet Animation",
+    description: "A simple sprite sheet animation playground",
+  },
+];
+
+function createDivCard(title, description, gameKey) {
+  const imageUrl = `assets/${gameKey}.png`;
+  const link = `${gameKey}/index.html`;
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+        <div class="card-image">
+        <img src="${imageUrl}" alt="${title}">
+        </div>
+        <div class="card-content">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        </div>
+        <a class="demo-btn" href="${link}" target="_blank">Demo</a>
+    `;
+  return card;
+}
+
+window.onload = function () {
+  const projects = document.getElementById("projects");
+    projectList.forEach((project) => {
+        projects.appendChild(createDivCard(project.name, project.description, project.gameKey))
+    });
+};
